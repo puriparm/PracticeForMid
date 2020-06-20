f8c79924588c907a6632041596c06b2d07e6926b
puriparm <puriparm@sherdiancollege.ca>
2020-06-20 17:18:46 -0400

    1 commit

---
+++ src/practice1/Practice1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practice1;
+
+/**
+ *
+ * This class +++Insert Description Here+++
+ * 
+ * @author Parminder Kaur
+ */
+public class Practice1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is a good thing");
+    }
+
+}
